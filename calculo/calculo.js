ca8c6c6d3b0608c6ca5fcda8059a583ae1c25ee2
laribezk <143816978+laribezk@users.noreply.github.com>
2024-09-27 07:46:39 -0300

    Add files via upload

---
+++ calculo/calculo.js
@@ -0,0 +1,27 @@
+function adicao (){
+    resultado =  30+10;
+    console.log(resultado);
+}
+
+console.log(adicao());
+
+function subtracao(){
+    resultado = 100 - 22;
+    console.log(resultado);
+}
+
+console.log(subtracao());
+
+function divisao (){
+    resultado = 300 / 10;
+    console.log(resultado);
+}
+
+console.log(divisao());
+
+function multiplicacao(){
+    resultado = 20 *20;
+    console.log(resultado);
+}
+
+console.log(multiplicacao());
